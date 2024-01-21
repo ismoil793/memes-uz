bd6b2992f28a38d895dcea9aaffd1252bbedb1c6
Ismoil Shokirov <ismoil.793@gmail.com>
2024-01-21 17:49:14 -0600

    Meme Generator

---
+++ next.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  webpack: (config) => {
+    // camelCase style names from css modules
+    config.module.rules
+      .find(({ oneOf }) => !!oneOf)
+      .oneOf.filter(({ use }) => JSON.stringify(use)?.includes("css-loader"))
+      .reduce((acc, { use }) => acc.concat(use), [])
+      .forEach(({ options }) => {
+        if (options.modules) {
+          options.modules.exportLocalsConvention = "camelCase";
+        }
+      });
+
+    return config;
+  },
+  reactStrictMode: false,
+};
---
+++ src/components/Meme/MemeGenerator.tsx
@@ -0,0 +1,190 @@
+"use client";
+import React, {
+  useState,
+  ChangeEvent,
+  useEffect,
+  useCallback,
+  useRef,
+} from "react";
+import styles from "./styles.module.scss";
+import Image from "next/image";
+import axios from "axios";
+
+interface Meme {
+  id: number;
+  name: string;
+  url: string;
+  width: number;
+  height: number;
+  box_count: number;
+  captions: number;
+}
+
+interface TopTextPosition {
+  top: number;
+  left: number;
+}
+
+interface BottomTextPosition {
+  bottom: number;
+  left: number;
+}
+
+const MemeGenerator = () => {
+  const [textTop, setTextTop] = useState<string>("");
+  const [textBottom, setTextBottom] = useState<string>("");
+  const [randomMeme, setRandomMeme] = useState<Meme | null>(null);
+  const [memes, setMemes] = useState<Meme[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [topTextPosition, setTopTextPosition] = useState<TopTextPosition>({
+    top: 15,
+    left: 15,
+  });
+  const [bottomTextPosition, setBottomTextPosition] =
+    useState<BottomTextPosition>({
+      bottom: 30,
+      left: 15,
+    });
+
+  const onTextTopChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTextTop(e.target.value);
+  };
+
+  const onTextBottomChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTextBottom(e.target.value);
+  };
+
+  const fetchMemes = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get("https://api.imgflip.com/get_memes");
+      setMemes(response.data.data.memes);
+      setRandomMeme(response.data.data.memes[0]);
+    } catch (e: any) {
+      console.error(e);
+      alert(`Error while fetching memes: ${e.message}`);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  const genRandomMeme = useCallback(() => {
+    const randomNumber = Math.floor(Math.random() * memes.length);
+    const randMeme = memes[randomNumber];
+    setRandomMeme(randMeme);
+  }, [memes]);
+
+  const onTopTextPositionChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTopTextPosition((prev) => ({
+      ...prev,
+      [e.target.name]: Number(e.target.value),
+    }));
+  };
+
+  const onBottomTextPositionChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setBottomTextPosition((prev) => ({
+      ...prev,
+      [e.target.name]: Number(e.target.value),
+    }));
+  };
+
+  useEffect(() => {
+    fetchMemes();
+  }, []);
+
+  return (
+    <div className={styles.memeGenerator}>
+      <header className={styles.memeHeader}>
+        <Image width={100} height={80} src="/troll.png" alt="Troll face" />
+        <h1>Meme Generator</h1>
+      </header>
+
+      <div className={styles.memeGenWrapper}>
+        <div className={styles.memeRandom}>
+          <div className={styles.memeImgWrap}>
+            {isLoading ? (
+              <div>Loading...</div>
+            ) : (
+              <figure>
+                <img src={randomMeme?.url} alt="Random meme" />
+                <figcaption>{randomMeme?.name}</figcaption>
+              </figure>
+            )}
+            <h2 className={styles.textTop} style={{ ...topTextPosition }}>
+              {textTop}
+            </h2>
+            <h2 className={styles.textBottom} style={{ ...bottomTextPosition }}>
+              {textBottom}
+            </h2>
+          </div>
+          <div onClick={genRandomMeme} className={styles.memeBtn}>
+            Copy meme
+          </div>
+        </div>
+
+        <div className={styles.memeModerateArea}>
+          <div>
+            <input
+              placeholder="Top Text"
+              type="text"
+              value={textTop}
+              style={{width: 'calc(100% - 200px)'}}
+              onChange={onTextTopChange}
+            />
+            <input
+              type="number"
+              placeholder="top"
+              value={topTextPosition.top}
+              name="top"
+              style={{width: 100}}
+              onChange={onTopTextPositionChange}
+            />
+            <input
+              type="number"
+              placeholder="left"
+              name="left"
+              style={{width: 100}}
+              value={topTextPosition.left}
+              onChange={onTopTextPositionChange}
+            />
+          </div>
+
+          <div>
+            <input
+              placeholder="Bottom Text"
+              type="text"
+              value={textBottom}
+              style={{width: 'calc(100% - 200px)'}}
+              onChange={onTextBottomChange}
+            />
+            <input
+              type="number"
+              placeholder="bottom"
+              name="bottom"
+              style={{width: 100}}
+              value={bottomTextPosition.bottom}
+              onChange={onBottomTextPositionChange}
+            />
+            <input
+              type="number"
+              placeholder="left"
+              name="left"
+              style={{width: 100}}
+              value={bottomTextPosition.left}
+              onChange={onBottomTextPositionChange}
+            />
+          </div>
+
+          <div
+            onClick={genRandomMeme}
+            className={`${styles.memeBtn} gen-random-meme`}
+          >
+            Gen Random Meme
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MemeGenerator;
