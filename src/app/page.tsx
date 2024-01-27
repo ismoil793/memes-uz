fdb3f1d34e3c48e83930a50af1535fcca7237e44
Ismoil Shokirov <ismoil.793@gmail.com>
2024-01-26 23:37:07 -0600

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+import { CSSProperties } from 'react';
 import MemeGenerator from '@/components/Meme/MemeGenerator';
 
-export default function Test() {
+interface IMemePageStyle {
+  container: CSSProperties;
+  footer: CSSProperties;
+}
+
+export default function MemePage() {
   return (
-    <div className="meme-container">
+    <div className="meme-container" style={styles.container}>
       <MemeGenerator />
+      <footer style={styles.footer}>
+        &copy; <a href="https://shokirov.uz">https://shokirov.uz</a>
+      </footer>
     </div>
   );
 }
+
+const styles: IMemePageStyle = {
+  container: {
+    position: 'relative'
+  },
+  footer: {
+    position: 'absolute',
+    left: '50%',
+    bottom: 0,
+    transform: 'translateX(-50%)'
+  }
+};
